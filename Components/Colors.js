c2f3ce9ce793a4d5f1c0cd8bf9f8eb54562da3ff
Zafer Çelik <zafer.celik06@hotmail.com>
2025-06-23 16:54:25 +0300

    Add base color palette

---
+++ Components/Colors.js
@@ -0,0 +1,12 @@
+import React from 'react';
+const Colors = {
+    primary: '#247BC7',
+    secondary: '#F5F7FA',
+    accent: '#5D3FD3',
+    textPrimary: '#1C1C1E',
+    textSecondary: '#5E5E5E',
+    white: '#FFFFFF',
+    danger: '#E63946',
+};
+
+export default Colors;  
