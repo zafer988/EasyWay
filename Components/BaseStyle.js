b23d2154f7152ac4c0ebb11d21dd39300629ea0c
Zafer Çelik <zafer.celik06@hotmail.com>
2025-06-23 16:54:39 +0300

    Add reusable base styles

---
+++ Components/BaseStyle.js
@@ -0,0 +1,20 @@
+export const main = {
+    alignItems: 'center',
+    justifyContent: 'center',
+
+}
+export const textCenter = {
+    alignItems: 'center',
+    justifyContent: 'center',
+    textAlign: 'center',
+}
+export const shadow = {
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.2,
+    shadowRadius: 6,
+};
+export const titleText = {
+    fontWeight: 'bold',
+    fontSize: 28
+}
